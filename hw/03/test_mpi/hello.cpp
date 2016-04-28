361472df140e4e830c633458e7096c8fa2a39f3b
Arthur Lui <luiarthur@gmail.com>
2016-04-28 15:32:35 -0700

    yes

---
+++ hw/03/test_mpi/hello.cpp
@@ -0,0 +1,13 @@
+#include "mpi.h"
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+  int rank, size;
+  MPI::Init(argc, argv);
+  rank = MPI::COMM_WORLD.Get_rank();
+  size = MPI::COMM_WORLD.Get_size();
+  cout << "Hello, world! I am process " << rank << " of " << size << endl;
+  MPI::Finalize();
+  return 0;
+}
